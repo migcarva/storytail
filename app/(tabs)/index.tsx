b50eaace9030725f925934b5c74f72e1933be591
Miguel Carvalho <m@miguelcarvalho.xyz>
2024-02-13 20:14:56 +0000

    clean up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,6 @@ export default function UserLibraryScreen() {
   return (
     <View f={1} alignItems='center' justifyContent='center' backgroundColor="$accent">
       <Text fontSize="$2" color="$white" fontFamily="$heading">User Library</Text>
-      <Text style={{ fontFamily: 'Bellota_700Bold', fontSize: 40 }}>Inter Black</Text>
     </View>
   );
 }
