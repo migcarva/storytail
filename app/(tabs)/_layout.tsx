297d48135561f195734e309397546f28aa1d2c90
Miguel Carvalho <m@miguelcarvalho.xyz>
2024-02-14 23:03:42 +0000

    fix error

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,41 +7,42 @@ import { TabBarIcon } from '@/components/TabBarIcon';
 import { AppHeaderLeft } from '@/components/AppHeaderLeft';
 import { AppHeaderRight } from '@/components/AppHeaderRight';
 
-const screenOptions = {
-  // Disable the static render of the header on web
-  // to prevent a hydration error in React Navigation v6.
-  headerShown: useClientOnlyValue(false, true),
-  headerTransparent: true,
-  headerShadowVisible: false, // removes the "border" on the header
-  headerTitle: '',
-  tabBarStyle: {
-    backgroundColor: getTokens().color.accent.val,
-    borderBlockColor: 'transparent',
-  },
-  tabBarShowLabel: false,
-  tabBarActiveTintColor: getTokens().color.white.val,
-  tabBarInactiveTintColor: getTokens().color.lightgrey.val,
-};
-
 export default function TabLayout() {
-  return (
-    <Tabs screenOptions={screenOptions}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Your library',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home-outline" color={color} />,
-          headerLeft: () => <AppHeaderLeft />,
-          headerRight: () => <AppHeaderRight />,
-        }}
-      />
-      <Tabs.Screen
-        name="online-library"
-        options={{
-          title: 'Online Library',
-          tabBarIcon: ({ color }) => <TabBarIcon name="globe-outline" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  const screenOptions = {
+    // Disable the static render of the header on web
+    // to prevent a hydration error in React Navigation v6.
+    headerShown: useClientOnlyValue(false, true),
+    headerTransparent: true,
+    headerShadowVisible: false, // removes the "border" on the header
+    headerTitle: '',
+    tabBarStyle: {
+      backgroundColor: getTokens().color.accent.val,
+      borderBlockColor: 'transparent',
+    },
+    tabBarShowLabel: false,
+    tabBarActiveTintColor: getTokens().color.white.val,
+    tabBarInactiveTintColor: getTokens().color.lightgrey.val,
+  };
+return (
+  <Tabs screenOptions={screenOptions}>
+    <Tabs.Screen
+      name="index"
+      options={{
+        title: 'Your library',
+        tabBarIcon: ({ color }) => <TabBarIcon name="home-outline" color={color} />,
+        headerLeft: () => <AppHeaderLeft />,
+        headerRight: () => <AppHeaderRight />,
+      }}
+    />
+    <Tabs.Screen
+      name="online-library"
+      options={{
+        title: 'Online Library',
+        tabBarIcon: ({ color }) => <TabBarIcon name="globe-outline" color={color} />,
+        headerLeft: () => <AppHeaderLeft />,
+        headerRight: () => <AppHeaderRight />,
+      }}
+    />
+  </Tabs>
+);
 }
