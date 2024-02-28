3c3114f134f5507b54b3635175d8e42291207d88
Miguel Carvalho <m@miguelcarvalho.xyz>
2024-02-28 11:17:49 +0000

    singout flow

--- app/(auth)/settings.tsx
+++ app/(auth)/settings.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 import SignoutButton from '@/src/components/navs/SignoutButton';
+import { useSupabase } from '@/src/hooks/useSupabase';
 
 const Settings: React.FC = () => {
-  // const { user } = useUser();
+  const { user, signOut } = useSupabase();
+  console.log(user);
 
   return (
     <View className="flex flex-1 items-center justify-center bg-background">
-      <Text className="text-xl text-white">
+      <Text className="text-xl text-black">
         {/* Welcome, {user?.firstName} {user?.lastName} 🎉 */}
         Yo!
       </Text>
--- src/components/navs/SignoutButton.tsx
+++ src/components/navs/SignoutButton.tsx
@@ -1,17 +1,18 @@
-import { useAuth } from '@clerk/clerk-expo';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Pressable } from 'react-native';
+import { Pressable, Text } from 'react-native';
+
+import { useSupabase } from '@/src/hooks/useSupabase';
 
 const SignoutButton: React.FC = () => {
-  const { signOut } = useAuth();
-
-  const doLogout = () => {
-    signOut();
-  };
+  const { signOut } = useSupabase();
 
   return (
-    <Pressable onPress={doLogout} style={{ marginRight: 10 }}>
+    <Pressable onPress={signOut} style={{ marginRight: 10 }}>
       <Ionicons name="log-out-outline" size={24} className="text-black" />
+      <Text className="text-xl text-black">
+        {/* Welcome, {user?.firstName} {user?.lastName} 🎉 */}
+        Sign out
+      </Text>
     </Pressable>
   );
 };
